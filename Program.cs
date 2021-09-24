48c6bdec1a78dff0940034b7533d50b0b469f00b
claraherz <stocklermah@outlook.com>
2021-09-24 14:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace aulaIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exec 01 ----------------------
+            //int a =  int.Parse(Console.ReadLine());
+
+            //if (a > 10)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não é maior que 10");
+            //}
+
+            //exec 02 ----------------------
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //if (a == b)
+            //{
+            //    Console.WriteLine("são iguais");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("são diferentes");
+            //}
+
+            //exec 03 ----------------------
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(b);
+            //}
+
+            //exec 04 ----------------------
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //if (b > a && b > c)
+            //{
+            //    Console.WriteLine(b);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+            //exec 05 ----------------------
+            //int a = int.Parse(Console.ReadLine());
+
+            //switch (a)
+            //{
+            //    case 1: Console.WriteLine(a);
+            //        break;
+            //    case 2: Console.WriteLine(a);
+            //        break;
+            //    case 3: Console.WriteLine(a);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Não computável");
+            //        break;
+            //}
+
+            //exec 06 ----------------------
+            //Random ran = new Random();
+            //int resultado;
+            //resultado = ran.Next(1,7);
+
+            //if (resultado >= 3)
+            //{
+            //    Console.WriteLine("Você ganhou");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Você perdeu");
+            //}
+
+            //exec 07 ----------------------
+            //int a = int.Parse(Console.ReadLine());
+
+            //a = a % 2;
+
+            //if (a == 0)
+            //{
+            //    Console.WriteLine("Seu numero é par");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Seu numero é impar");
+            //}
+
+            //exec 08 ----------------------
+            //double valorDaCompra, valorcomDesconto;
+
+            //valorDaCompra = double.Parse(Console.ReadLine());
+
+            //if (valorDaCompra > 254.50)
+            //{
+            //    valorcomDesconto = valorDaCompra * 0.72;
+            //    Console.WriteLine(valorDaCompra);
+            //    Console.WriteLine(valorcomDesconto);
+            //}
+            //else
+            //{
+            //    valorcomDesconto = valorDaCompra * 0.79;
+            //    Console.WriteLine(valorDaCompra);
+            //    Console.WriteLine(valorcomDesconto);
+            //}
+
+            //exec 09 ----------------------
+            //double salario = double.Parse(Console.ReadLine());
+            //double aumento;
+            //if (salario <= 600)
+            //{
+            //    aumento = salario * 0.30;
+            //}
+            //else if (salario > 600 && salario <= 1.100)
+            //{
+            //    aumento = salario * 0.25;
+            //}
+            //else if (salario > 1.100 && salario <= 2.400)
+            //{
+            //    aumento = salario * 0.20;
+            //}
+            //else if (salario > 2.400 && salario <= 3.500)
+            //{
+            //    aumento = salario * 0.15;
+            //}
+            //else
+            //{
+            //    aumento = salario * 0.10;
+            //}
+
+            //Console.WriteLine(salario + aumento);
+        }
+    }
+}
